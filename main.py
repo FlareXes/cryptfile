45422f27d6163b0975a6bb6df84e57cfb86b39cc
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-09-23 11:04:01 +0530

    Structured The Project

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-from obsfile import security
-from obsfile import storage
-
-
-def encrypt_file(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    enc_data = security.encrypt(data, input("Password: "))
-    storage.save_file(enc_data, file)
-
-
-def decrypt_file(enc_file):
-    enc_data = storage.open_file(enc_file)
-    dec_data = security.decrypt(enc_data, input("Password: "))
-    with open(enc_file.split(".enc")[0], "wb") as f:
-        f.write(dec_data)
-
+from obfile import storage
 
 if __name__ == '__main__':
-    encrypt_file("file.txt")
-    decrypt_file("file.txt.enc")
+    storage.encrypt_file("last_pic.png")
+    storage.decrypt_file("pic.png.enc")
--- obfile/storage.py
+++ obfile/storage.py
@@ -1,5 +1,7 @@
 import pickle
 
+from obfile import security
+
 
 def save_file(data, file):
     with open(file + ".enc", "wb") as f:
@@ -10,3 +12,17 @@ def open_file(file):
     with open(file, "rb") as f:
         data = pickle.load(f)
     return data
+
+
+def encrypt_file(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    enc_data = security.encrypt(data, input("Password: "))
+    save_file(enc_data, file)
+
+
+def decrypt_file(enc_file):
+    enc_data = open_file(enc_file)
+    dec_data = security.decrypt(enc_data, input("Password: "))
+    with open(enc_file.split(".enc")[0], "wb") as f:
+        f.write(dec_data)
