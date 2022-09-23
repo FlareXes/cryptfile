bd56dfaf5ccd3987a63ebc0b40526a09ea187c90
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-09-23 11:47:35 +0530

    Added Args Support And No Prompt Password Input

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import argparse
+import sys
+
 from obfile import storage
 
 if __name__ == '__main__':
-    storage.encrypt_file("last_pic.png")
-    storage.decrypt_file("pic.png.enc")
+    parser = argparse.ArgumentParser(description='Command line utility to encrypt or decrypt the file with AES256')
+    parser.add_argument('-e', '--encrypt', nargs=1, help="encrypt the specified file", type=storage.encrypt_file)
+    parser.add_argument('-d', '--decrypt', nargs=1, help="decrypt the specified file", type=storage.decrypt_file)
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
--- obfile/storage.py
+++ obfile/storage.py
@@ -1,7 +1,6 @@
 import pickle
-
 from obfile import security
-
+from getpass import getpass
 
 def save_file(data, file):
     with open(file + ".enc", "wb") as f:
@@ -17,12 +16,12 @@ def open_file(file):
 def encrypt_file(file):
     with open(file, "rb") as f:
         data = f.read()
-    enc_data = security.encrypt(data, input("Password: "))
+    enc_data = security.encrypt(data, getpass("Password: "))
     save_file(enc_data, file)
 
 
 def decrypt_file(enc_file):
     enc_data = open_file(enc_file)
-    dec_data = security.decrypt(enc_data, input("Password: "))
+    dec_data = security.decrypt(enc_data, getpass("Password: "))
     with open(enc_file.split(".enc")[0], "wb") as f:
         f.write(dec_data)
