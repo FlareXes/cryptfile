4fd6437f8b18f4e95cbd77da2c70c398796ef0c9
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-09-24 09:27:53 +0530

    Added -r Arg To Remove Original File

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import argparse
 import sys
-
+import argparse
 from obfile import storage
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Command line utility to encrypt or decrypt the file with AES256')
-    parser.add_argument('-e', '--encrypt', nargs=1, help="encrypt the specified file", type=storage.encrypt_file)
-    parser.add_argument('-d', '--decrypt', nargs=1, help="decrypt the specified file", type=storage.decrypt_file)
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Command line utility to encrypt or decrypt the file with AES256.")
+    parser.add_argument("-e", "--encrypt", nargs=1, action="store", help="encrypt the specified file", type=str)
+    parser.add_argument("-d", "--decrypt", nargs=1, help="decrypt the specified file", type=str)
+    parser.add_argument("-r", "--remove", action="store_true", help="delete original file after any operation")
     args = parser.parse_args()
 
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
+    if len(sys.argv) == 1: parser.print_help(); sys.exit(1)
+
+    try:
+        if len(args.encrypt) == 1 and args.decrypt is None:
+            storage.encrypt_file(args.encrypt[0], args.remove)
+    except TypeError:
+        if args.encrypt is None and len(args.decrypt) == 1:
+            storage.decrypt_file(args.decrypt[0], args.remove)
--- obfile/storage.py
+++ obfile/storage.py
@@ -1,6 +1,8 @@
+import os
 import pickle
 from obfile import security
 from getpass import getpass
+
 
 def save_file(data, file):
     with open(file + ".enc", "wb") as f:
@@ -13,15 +15,21 @@ def open_file(file):
     return data
 
 
-def encrypt_file(file):
+def remove_file(file):
+    os.remove(file)
+
+
+def encrypt_file(file, remove_original: bool = False):
     with open(file, "rb") as f:
         data = f.read()
     enc_data = security.encrypt(data, getpass("Password: "))
     save_file(enc_data, file)
+    if remove_original: remove_file(file)
 
 
-def decrypt_file(enc_file):
+def decrypt_file(enc_file, remove_original: bool = False):
     enc_data = open_file(enc_file)
     dec_data = security.decrypt(enc_data, getpass("Password: "))
     with open(enc_file.split(".enc")[0], "wb") as f:
         f.write(dec_data)
+    if remove_original: remove_file(enc_file)
