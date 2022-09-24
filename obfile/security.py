9c5ca073c6204f9ad08db156c3786873c77d2aca
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-09-24 13:38:56 +0530

    Updated Scrypt Security Parameters

--- obfile/security.py
+++ obfile/security.py
@@ -5,7 +5,7 @@ from Cryptodome.Random import get_random_bytes
 
 def encrypt(data, key):
     random_bytes = get_random_bytes(32).__str__()
-    private_key = scrypt(key, random_bytes, 32, 2 ** 14, 8, 1)
+    private_key = scrypt(password=key, salt=random_bytes, key_len=32, N=2 ** 20, r=8, p=1)
 
     cipher = AES.new(private_key, AES.MODE_GCM)
     ciphertext, tag = cipher.encrypt_and_digest(data)
@@ -15,13 +15,12 @@ def encrypt(data, key):
 
 
 def decrypt(configs, key):
-    random_bytes = get_random_bytes(32).__str__()
     random_bytes = configs['salt']
     ciphertext = configs['ciphertext']
     nonce = configs['nonce']
     tag = configs['tag']
 
-    private_key = scrypt(key, random_bytes, 32, 2 ** 14, 8, 1)
+    private_key = scrypt(password=key, salt=random_bytes, key_len=32, N=2 ** 20, r=8, p=1)
     cipher = AES.new(private_key, AES.MODE_GCM, nonce)
 
     data = cipher.decrypt_and_verify(ciphertext, tag)
